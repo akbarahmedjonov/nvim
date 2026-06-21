51503a5de066c816dc1b15f1be3f80373465dfb7
akbarahmedjonov <teamender1991@gmail.com>
2026-06-21 10:13:04 +0500

    refactor to gruvbox

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,6 +9,8 @@ set("n", "<leader>cd", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 set("n", "<leader>ff", "<CMD>Telescope fd<CR>", { desc = "Telescope find files" })
 set("n", "<leader>fb", "<CMD>Telescope buffers<CR>", { desc = "Telescope find buffers" })
 set("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", { desc = "Telescope grep strings" })
+set("n", "<leader>fr", "<CMD>Telescope oldfiles<CR>", { desc = "Telescope recent files" })
+
 set("n", "<leader>u", "<CMD>Undotree<CR>", { desc = "Toggle undotree" })
 
 set("v", "J", ":m '>+1<CR>gv=gv") -- Alt Up/Down in vscode
